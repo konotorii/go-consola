72b91764ae5e93f5f9aad735e69c4946a030fb4b
Konotorii <79553360+konotorii@users.noreply.github.com>
2024-06-13 09:19:04 -0400

    feat: 6 Basic functions

--- consola.go
+++ consola.go
@@ -2,24 +2,30 @@ package consola
 
 import (
 	"fmt"
+	"github.com/TwiN/go-color"
+	"time"
 )
 
 func log(msg ...any) {
-	fmt.Println(color.Ize(color.Red, ""))
+	fmt.Println(color.Ize(color.White, time.Now().Format(time.RFC3339)), "[LOG]", msg)
 }
 
-func info() {
-	fmt.Println("")
+func info(msg ...any) {
+	fmt.Println(color.Ize(color.White, time.Now().Format(time.RFC3339)), "[INFO]", msg)
 }
 
-func debug() {
-	fmt.Println("")
+func debug(msg ...any) {
+	fmt.Println(color.Ize(color.White, time.Now().Format(time.RFC3339)), color.Ize(color.Gray, "[DEBUG]"), msg)
 }
 
-func warning() {
-	fmt.Println("")
+func success(msg ...any) {
+	fmt.Println(color.Ize(color.White, time.Now().Format(time.RFC3339)), color.Ize(color.Green, "[SUCCESS]"), msg)
 }
 
-func error() {
-	fmt.Println("")
+func warning(msg ...any) {
+	fmt.Println(color.Ize(color.White, time.Now().Format(time.RFC3339)), color.Ize(color.Yellow, "[WARNING]"), msg)
+}
+
+func error(msg ...any) {
+	fmt.Println(color.Ize(color.White, time.Now().Format(time.RFC3339)), color.Ize(color.Red, "[ERROR]"), msg)
 }
